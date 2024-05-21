13d21c6b2c1bda50a7a6cf6fa2cb9db8bbc07e5b
Trond Trosterud <trond.trosterud@uit.no>
2024-05-21 09:10:22 +0200

    test file for generating paradigms for random words.

---
+++ devtools/noun_minip.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# script to generate paradigms for generating word forms
+# command:
+# sh generate_contlex_para.sh PATTERN
+# example, when you are in khk:
+# sh devtools/noun_minip.sh n_21 | less
+# sh devtools/noun_minip.sh järvenpää 
+# Only get the lemma you ask for:
+# sh devtools/noun_minip.sh '^pää[ :+]' 
+
+
+LOOKUP=$(echo $LOOKUP)
+HLOOKUP=$(echo $HLOOKUP)
+GTLANGS=$(echo $GTLANGS)
+
+
+PATTERN=$1
+L_FILE="in.txt"
+cut -d '!' -f1 src/fst/morphology/stems/nouns.lexc | egrep $PATTERN | sed 's/% /%/g' | tr ' +' ':' | cut -d ':' -f1 | sed 's/%/% /g' | tr -d '%'>$L_FILE
+
+
+P_FILE="test/data/testnounparadigm.txt"
+
+for lemma in $(cat $L_FILE);
+do
+ for form in $(cat $P_FILE);
+ do
+#  echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-khk/src/generator-gt-norm.xfst
+  echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-khk/src/fst/generator-gt-norm.hfstol
+ done
+ rm -f $L_FILE
+done
+
+

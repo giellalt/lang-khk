f0c8ed9d2af65e07773f992cdae6406b52cd61b9
Trond Trosterud <trond.trosterud@uit.no>
2025-02-03 09:25:01 +0100

    hfst

--- devtools/noun_minip.sh
+++ devtools/noun_minip.sh
@@ -26,7 +26,7 @@ for lemma in $(cat $L_FILE);
 do
  for form in $(cat $P_FILE);
  do
-#  echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-khk/src/generator-gt-norm.xfst
+#  echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-khk/src/fst/generator-gt-norm.xfst
   echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-khk/src/fst/generator-gt-norm.hfstol
  done
  rm -f $L_FILE
